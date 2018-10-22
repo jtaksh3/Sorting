1007837536d448039c88b1013a084a0a0501b4e3
Aakash Choudhary <42690551+jtaksh3@users.noreply.github.com>
2018-10-22 22:46:44 +0530

    Update and rename SelectionSort.java to InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SelectionSort {
+public class InsertionSort {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter no. of elements to be Sorted :");
